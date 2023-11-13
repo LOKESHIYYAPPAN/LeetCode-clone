4c56893dbe1b7e1b014d7cc8acd33d72b3028bba
LOKESH I <148192887+LOKESHIYYAPPAN@users.noreply.github.com>
2023-11-13 07:38:59 +0530

    Add files via upload

---
+++ sign in.js
@@ -0,0 +1,12 @@
+function btn1(){
+    const downone = document.querySelector(".downone")
+    downone.classList.toggle("show")
+    const btn1 = document.querySelector(".btn1 span")
+    btn1.classList.toggle("btn-span")
+}
+function btn2(){
+    const downone = document.querySelector(".downtwo")
+    downone.classList.toggle("show")
+    const btn2 = document.querySelector(".btn2 span")
+    btn2.classList.toggle("btn-span")
+}
